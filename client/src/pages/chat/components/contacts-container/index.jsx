1d7b759695d3e462bf5b731dbd145b46d9c0edd3
Chinu2522 <mishrachinmayee963@gmail.com>
2024-08-28 07:32:32 +0530

    debugging

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -20,7 +20,7 @@ const ContactsContainer = () => {
     }
 
     getContacts();
-  },[])
+  },[setDirectMessagesContacts])
   return (
     <div className="relative md:w-[35vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
       <div className="pt-3">
